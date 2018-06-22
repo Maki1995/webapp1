fddb0e7909845eb571c0a472a68e93de1ed21dbd
Maki1995 <1182841679@qq.com>
2018-06-23 00:08:54 +0800

    测试修改上传至GitHub

---
+++ src/com/itheima/test/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Integer value = 0;
+        System.out.println("------Maki------value值=" + value + "," + "当前类=.()");
+    }
+
+    private Integer value2;
+}
